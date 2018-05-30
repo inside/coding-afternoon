66109c3be639b60957b8fc87a5dd4eed3ff25d5c
Yann Thomas-Gérard <inside@gmail.com>
2018-05-30 22:01:02 +0200

    add first non working version of the birthday program

---
+++ birthday/README.md
@@ -0,0 +1,10 @@
+# Birthday
+
+* Start the program with
+
+./index.js
+Notice there's a syntax error.
+
+* To locate and fix the error, start vim with:
+
+vim -c 'syntax off' -c 'set nonumber norelativenumber' index.js
---
+++ birthday/index.js
@@ -0,0 +1,50 @@
+#!/usr/bin/env node
+
+// Alexandre
+if ('06-09' == '09-30') {
+  console.log(`Aujourd'hui, nous somme le ${today}.`)
+  console.log(`C'est l'anniversaire de Alexandre, YOUPI !!`)
+  console.log(`!! JOYEUX ANIVERSAIRE Alexandre !!`)
+}
+
+// Camille
+if ('06-09' == '03-02') {
+  console.log(`Aujourd'hui, nous somme le ${today}.)
+  console.log(`C'est l'anniversaire de Camille, YOUPI !!`)
+  console.log(`!! JOYEUX ANNIVERSAIRE Camille !!`)
+}
+
+// Esther
+if ('06-09' == '') {
+  console.log(`Aujourd'hui, nous somme le ${today}.`)
+  console.log(`C'est l'anniversaire de Esther, YOUPI !!`)
+  console.log(`!! JOYEUX ANNIVERSAIRE Esther !!`)
+}
+
+// Joanne
+if ('06-09' == '') {
+  console.log(`Aujourd'hui, nous somme le ${today}.`)
+  console.log(`C'est l'anniversaire de Joanne, YOUPI !!`)
+  console.log(`!! JOYEUX ANNIVERSAIRE Joanne !!`)
+}
+
+// Jean
+if ('06-09' == '') {
+  console.log(`Aujourd'hui, nous somme le ${today}.`)
+  console.log(`C'est l'anniversaire de Jean, YOUPI !!`)
+  console.log(`!! JOYEUX ANNIVERSAIRE Jean !!`)
+}
+
+// Castille
+if ('06-09' == '') {
+  console.log(`Aujourd'hui, nous somme le ${today}.`)
+  console.log(`C'est l'anniversaire de Castille, YOUPI !!`)
+  console.log(`!! JOYEUX ANNIVERSAIRE Castille !!`)
+}
+
+// Léonard
+if ('06-09' == '') {
+  console.log(`Aujourd'hui, nous somme le ${today}.`)
+  console.log(`C'est l'anniversaire de Léonard, YOUPI !!`)
+  console.log(`!! JOYEUX ANNIVERSAIRE Léonard !!`)
+}
